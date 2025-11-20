9b1203f46efef97fd77d5d568929fa09979817d9
Weiming Wang <weimingw24@gmail.com>
2025-11-19 19:43:27 -0500

    final passing isTriangle function (TDD GREEN)

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -73,5 +73,13 @@ int* getTriangleSides(int* triangleSides) {
 
 int isTriangle(int side1, int side2, int side3)
 {
-	return side1 + side3;
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+		return 0;
+
+	if (side1 + side2 > side3 &&
+		side1 + side3 > side2 &&
+		side2 + side3 > side1)
+		return 1;
+
+	return 0;
 }
--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -1,5 +1,16 @@
-#pragma once
-void printWelcome();
-int printShapeMenu();
-int* getTriangleSides(int* triangleSides);
-char* isTriangle(int side1, int side2, int side3);
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int isTriangle(int side1, int side2, int side3);
+	int* getTriangleSides(int* triangleSides);
+		int printShapeMenu();
+		void printWelcome();
+#ifdef __cplusplus
+}
+#endif
+
+#endif
