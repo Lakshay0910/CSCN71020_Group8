bb2202f6fb1f0c3a1d89d0cb06f3cae91f8ff34e
Weiming Wang <weimingw24@gmail.com>
2025-11-19 19:41:00 -0500

    isTriangle function failling test (TDD RED)

--- isTriangle/isTriangle.cpp
+++ isTriangle/isTriangle.cpp
@@ -1,34 +1,53 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include ".../CSCN71020_Group8/main.h"
-extern "C" char* isTriangle(int side1, int side2, int side3);
+#include "../PolygonChecker/main.h"  
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace isTriangle
+namespace PolygonCheckerTest
 {
-	TEST_CLASS(isTriangle)
-	{
-	public:
+    TEST_CLASS(IsTriangleTests)
+    {
+    public:
 
-		TEST_METHOD(negativeNumbber)
-		{
-			char* result = isTriangle(-33, 23, 24);
-			Assert::AreEqual(result, "These line can't form a triangle. ");
-		}
-		TEST_METHOD(largeTriangle)
-		{
-			char* result = isTriangle(9340403, 3994933, 4234243434);
-			Assert::AreEqual(result, "These line can't form a triangle. ");
-		}
-		TEST_METHOD(twoLongOneShort)
-		{
-			char* result = isTriangle(99, 99, 1);
-			Assert::AreEqual(result, "These line can't form a triangle. ");
-		}
-		TEST_METHOD(zero)
-		{
-			char* result = isTriangle(10, 34, 0);
-			Assert::AreEqual(result, "These line can't form a triangle. ");
-		}
-	};
+        TEST_METHOD(NegativeNumber)
+        {
+            Assert::AreEqual(0, isTriangle(-33, 23, 24));
+        }
+
+        TEST_METHOD(LargeTriangleImpossible)
+        {
+            Assert::AreEqual(0, isTriangle(9340403, 3994933, 4234243434));
+        }
+
+        TEST_METHOD(TwoLongOneShort)
+        {
+            Assert::AreEqual(1, isTriangle(99, 99, 1));
+        }
+
+        TEST_METHOD(ZeroSide)
+        {
+            Assert::AreEqual(0, isTriangle(10, 34, 0));
+        }
+
+        TEST_METHOD(ValidTriangle)
+        {
+            Assert::AreEqual(1, isTriangle(3, 4, 5));
+        }
+
+        TEST_METHOD(EquilateralTriangle)
+        {
+            Assert::AreEqual(1, isTriangle(10, 10, 10));
+        }
+
+        TEST_METHOD(IsoscelesTriangle)
+        {
+            Assert::AreEqual(1, isTriangle(7, 7, 5));
+        }
+
+        TEST_METHOD(FlatLine)
+        {
+            Assert::AreEqual(0, isTriangle(5, 5, 10)); 
+        }
+    };
 }
